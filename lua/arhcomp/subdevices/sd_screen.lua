1f67c86bf1ac34c74897f7b16cb3784b4ff385ba
Stepan <sssstepa452@yandex.ru>
2021-04-03 23:24:34 +0300

    Screen SubDevice work

--- lua/arhcomp/subdevices/sd_screen.lua
+++ lua/arhcomp/subdevices/sd_screen.lua
@@ -1,14 +1,26 @@
 if SERVER then
+    local AllScreens = {}
+
     local SUBDEV = {}
 
     function SUBDEV:OnCreated(params)
-        print("Hello world")
+        self.AllScreensI = #AllScreens + 1
+
+        AllScreens[self.AllScreensI] = self
+
+        self.Position = params.Position
+        self.Normal = params.Normalize
+        self.Size = params.Size
+
     end
 
     function SUBDEV:OnRemoved()
-        print("Goodbye world")
+        AllScreens[self.AllScreensI] = nil
+    end
+
+    function SUBDEV:RenderDataDelta()
+
     end
 
     ArhComp.SubDevice.RegisterType("screen", SUBDEV)
-
 end
